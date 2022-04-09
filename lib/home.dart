0b318b87cfe25becc3be9be86fcdd87c869e0e4d
meehirmhatre1234 <meehirmhatre1234@gmail.com>
2022-04-09 15:40:08 +0530

    app

---
+++ lib/home.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:tflite/tflite.dart';
+import 'dart:math' as math;
+
+import 'camera.dart';
+import 'bndbox.dart';
+import 'models.dart';
+
+class HomePage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  HomePage(this.cameras);
+
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<dynamic> _recognitions;
+  int _imageHeight = 0;
+  int _imageWidth = 0;
+  String _model = "";
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  loadModel() async {
+    String res;
+    switch (_model) {
+      case yolo:
+        res = await Tflite.loadModel(
+          model: "assets/detect.tflite",
+          labels: "assets/labelmap.txt",
+        );
+        break;
+
+      case mobilenet:
+        res = await Tflite.loadModel(
+            model: "assets/mobilenet_v1_1.0_224.tflite",
+            labels: "assets/mobilenet_v1_1.0_224.txt");
+        break;
+
+      case posenet:
+        res = await Tflite.loadModel(
+            model: "assets/posenet_mv1_075_float_from_checkpoints.tflite");
+        break;
+
+      case detect:
+        res = await Tflite.loadModel(
+            model: "assets/detect.tflite",
+            labels: "assets/labelmap.txt");
+        break;
+
+      default:
+        res = await Tflite.loadModel(
+            model: "assets/ssd_mobilenet.tflite",
+            labels: "assets/ssd_mobilenet.txt");
+    }
+    print(res);
+  }
+
+  onSelect(model) {
+    setState(() {
+      _model = model;
+    });
+    loadModel();
+  }
+
+  setRecognitions(recognitions, imageHeight, imageWidth) {
+    setState(() {
+      _recognitions = recognitions;
+      _imageHeight = imageHeight;
+      _imageWidth = imageWidth;
+    });
+  }
+
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    Size screen = MediaQuery.of(context).size;
+    return Scaffold(
+      body: _model == ""
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: const Text(ssd),
+                    onPressed: () => onSelect(ssd),
+                  ),
+                  RaisedButton(
+                    child: const Text(yolo),
+                    onPressed: () => onSelect(yolo),
+                  ),
+                  RaisedButton(
+                    child: const Text(mobilenet),
+                    onPressed: () => onSelect(mobilenet),
+                  ),
+                  RaisedButton(
+                    child: const Text(posenet),
+                    onPressed: () => onSelect(posenet),
+                  ),
+                  RaisedButton(
+                    child: const Text(detect),
+                    onPressed: () => onSelect(detect),
+                  ),
+                ],
+              ),
+            )
+          : Stack(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height*0.7,
+                      child: Camera(
+                        widget.cameras,
+                        _model,
+                        setRecognitions,
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height*0.2,
+                      child: Text("$_recognitions"),
+                    )
+                  ],
+                ),
+                
+              ],
+            ),
+    );
+  }
+}
